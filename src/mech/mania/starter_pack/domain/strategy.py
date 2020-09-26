85a2a30a8e576f705bd35a60bac440cff8528443
Michael Zhou <mz25@illinois.edu>
2020-09-26 04:25:02 -0500

    finish astar

--- src/mech/mania/starter_pack/domain/strategy.py
+++ src/mech/mania/starter_pack/domain/strategy.py
@@ -5,6 +5,13 @@ from mech.mania.starter_pack.domain.model.characters.position import Position
 from mech.mania.starter_pack.domain.model.game_state import GameState
 from mech.mania.starter_pack.domain.api import API
 
+import heapq
+
+class Node:
+    def __init__(self, cost, prev, loc):
+        self.cost = cost
+        self.prev = prev
+        self.coord = loc
 
 class Strategy:
     def __init__(self, memory):
@@ -24,9 +31,8 @@ class Strategy:
         self.my_player = game_state.get_all_players()[player_name]
         self.board = game_state.get_pvp_board()
         self.curr_pos = self.my_player.get_position()
-        # self.monster_positions = self.game_state.get_monsters_on_board(self.board)
+        self.monster_positions = game_state.get_monsters_on_board("buffoon")
         self.logger.info(str(game_state.get_monsters_on_board("buffoon")))
-        # self.logger.info("Monster Position:{}".format(str(self.monster_positions[0])))
 
         # last_action, type = self.memory.get_value("last_action", str)
         # if last_action is not None and last_action == "PICKUP":
@@ -119,3 +125,59 @@ class Strategy:
         target_pos.create(target_pos.x, target_pos.y, target_pos.get_board_id())
         self.logger.info("yes2")
         return target_pos
+
+    def zhou_astar_path(self, player: Position, destination: Position) -> Position:
+        frontier = []
+        heapq.heapify(frontier)
+        start = Node(0, None, (player.x, player.y))
+        heapq.heappush(frontier, (0, (player.x, player.y)))
+        visited = set()
+        visited.add((player.x, player.y))
+        while frontier:
+            curr = heapq.heappop(frontier)[1]
+            if curr.coord == (destination.x, destination.y):
+                path = []
+                while curr:
+                    path.append(curr.coord)
+                    curr = curr.prev
+                path.reverse()
+                return path
+            neighbors = self.getNeighbors(curr.coord[0], curr.coord[1])
+            for n in neighbors:
+                if n not in visited:
+                    dist = abs(destination.y - n[1]) + abs(destination.x - n[0])
+                    newNode = Node(curr.cost + 1, curr, n)
+                    heapq.heappush(frontier, (newNode.cost + dist, newNode.coord))
+                    visited.add(n)
+        return []
+
+    def getNeighbors(self, getx, gety):
+        neighbors = []
+        for n in [(0,1), (0,-1), (-1,0), (1,0)]:
+            coord = (getx + n[0], gety + n[1])
+            if coord in self.obstacles or coord in self.bad_monster_squares:
+                continue
+            else:
+                neighbors.append(coord)
+        return neighbors
+
+    def get_obstacles(self, game_state):
+        grid = self.board.get_grid
+        self.obstacles = set()
+        for x_idx in range(len(grid)):
+            for y_idx in range(len(grid[x_idx])):
+                tile = self.board.get_tile_at(Position(x_idx, y_idx, "buffoon"))
+                if tile.TileType == "VOID" or tile.TileType == "IMPASSIBLE":
+                    print((x,y), tile.TileType)
+                    self.obstacles.add((x,y))
+        return self.obstacles:
+
+    def get_monsters(self, gameState, board_id):
+        monsters = game_state.get_monsters_on_board(board_id)
+        self.bad_monster_squares = set()
+        for monster in monsters:
+            m_p = monster.position
+            for direction in [(0,1), (0,-1), (-1,0), (1,0)]:
+                for step in range(monster.aggro_range):
+                    self.bad_monster_squares.add((direction[0] * step + monster.position.x , direction[1] * step + monster.position.y))
+        return self.bad_monster_squares
